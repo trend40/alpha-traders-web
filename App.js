82db98bd182bca0af620e28b07c9ac553fc5c710
trend40 <bethetrend007@gmail.com>
2025-07-20 14:12:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "60px" }}>
+      <h1>🚀 Welcome to Alpha Traders</h1>
+      <p>This is your trading & investing education platform.</p>
+    </div>
+  );
+}
+
+export default App;
